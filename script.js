dfee5def693c87225941f559bab0294263da80ca
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-07-01 22:40:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 function createTable() {
+	const table=document.getElementById('myTable')
     //Write your code here
 	  const rn=prompt("Input number of rows")
 	  const cn=prompt("Input number of columns")
 		let tableHTML=''
 		for(let i=0;i<rn;++i){
-			let tableCols=''
+			const tableRow=document.createElement('tr')
 			for(let j=0;j<cn;++j){
-				tableCols+=`<td> Row-${i} Column-${j} </td> `
+				const tableData=document.createElement('td')
+				tableData.textContent=`Row-${i} Column-${j}`
+				tableRow.append(tableData)
 			}
-			tableHTML+=`
-				<tr>
-					${tableCols}
-				</tr>
-			`
+			table.append(tableRow)
 		}
-
-	  document.getElementById('myTable').innerHTML=tableHTML
+	  
 }
+// function createTable() {
+//     //Write your code here
+// 	  const rn=prompt("Input number of rows")
+// 	  const cn=prompt("Input number of columns")
+// 		let tableHTML=''
+// 		for(let i=0;i<rn;++i){
+// 			let tableCols=''
+// 			for(let j=0;j<cn;++j){
+// 				tableCols+=`<td> Row-${i} Column-${j} </td> `
+// 			}
+// 			tableHTML+=`
+// 				<tr>
+// 					${tableCols}
+// 				</tr>
+// 			`
+// 		}
+
+// 	  document.getElementById('myTable').innerHTML=tableHTML
+// }
